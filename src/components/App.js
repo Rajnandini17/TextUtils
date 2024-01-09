d0d8c257c1d35e467ab1bef3fa8e706712e50f2f
Rajnandini Singh <rajnandini1702singh@gmail.com>
2024-01-09 14:24:24 +0530

    add dark mode switch

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import TextForm from "./TextForm";
-import About from "./About";
+// import About from "./About";
 // import ReactDOM from "react-dom";
 
 function App() {
+    const [mode, setMode] = useState('light');
+
+    const toggleMode = () => {
+        if(mode === 'light') {
+            setMode ('dark');
+            document.body.style.backgroundColor = '#042743';
+        } else {
+            setMode ('light');
+            document.body.style.backgroundColor = 'white';
+        }
+    }
     return (
         <>
-        <Navbar title="textUtils" aboutText="About"/>
+        <Navbar title="textUtils" aboutText="About" mode={mode} toggleMode={toggleMode}/>
         <div className="container my-3">
-        <TextForm heading="Enter the text to analyze"/>
+        <TextForm heading="Enter the text to analyze" mode={mode}/>
         </div>
-        <About />
+        {/* <About /> */}
         
-        </>
-           
+        </>      
     )
 };
 
